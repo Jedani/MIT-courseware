bff4ea741f46928c39cca43fc0d1611306a9585d
jedani <allansichangi35@gmail.com>
2022-08-28 23:13:49 +0300

    added structure

---
+++ Assignments/ps0.py
@@ -0,0 +1,9 @@
+#Problem set 0
+#Name: Allan Sichangi
+#Time: 1 : 00
+
+# A simple program to print out your name
+
+lastName = input("Enter your last name: ")
+firstName = input('Enter your first name: ')
+print("Your first name is " + firstName + " and last name is " + lastName)
