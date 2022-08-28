262505a00ff493e7a1b297ce4010cb6acf90c555
jedani <allansichangi35@gmail.com>
2022-08-28 23:43:28 +0300

    new

--- Assignments/ps0.py
+++ Assignments/ps0.py
@@ -6,4 +6,4 @@
 
 lastName = input("Enter your last name: ")
 firstName = input('Enter your first name: ')
-print("Your first name is " + firstName + " and last name is " + lastName)
+print("Your first name is " + firstName + " and last  name is " + lastName)
